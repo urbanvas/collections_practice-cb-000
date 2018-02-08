4a91e58a9bae00bd66115c837db12052204d4455
Neil Seejoor <nvseejoor@gmail.com>
2018-02-08 06:20:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+    end
+  end
+
+def sort_array_char_count(arr)
+  arr.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    else a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(arr)
+  temp = arr[1]
+  arr[1] = arr[2]
+  arr[2] = temp
+  return [arr[0], arr[1], arr[2]}
+end
